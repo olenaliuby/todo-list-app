c0419d22425aff335090c0ed731b459ed8bd7409
Olena Liubynetska <olena.liuby@gmail.com>
2023-08-04 18:09:54 +0100

    refactor(models): Tags in Task model now can be null

--- todo_app/models.py
+++ todo_app/models.py
@@ -13,7 +13,7 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     is_done = models.BooleanField(default=False)
-    tags = models.ManyToManyField(Tag, related_name="tasks")
+    tags = models.ManyToManyField(Tag, related_name="tasks", null=True, blank=True)
 
     class Meta:
         ordering = ["is_done", "-created_at"]
